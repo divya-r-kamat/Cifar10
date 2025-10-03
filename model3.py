e6bfe2815f911a3b70fbd765b3dcf7cbf051f703
Divya Kamat <42609155+divya-r-kamat@users.noreply.github.com>
2025-10-03 16:22:08 +0530

    Create model3.py

---
+++ model3.py
@@ -0,0 +1,106 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class CIFAR10(nn.Module):
+    def __init__(self):
+        super(CIFAR10, self).__init__()
+        self.drop_prob = 0.01
+        ## Convolution Block1
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(3, 32, 3, padding=1, bias = False),  # Input: 32x32x3 | Output: 32x32x32 | RF: 3x3
+            nn.ReLU(),
+            nn.BatchNorm2d(32),
+            nn.Dropout2d(self.drop_prob),
+
+            nn.Conv2d(32, 64, 3, padding=1, bias = False), # Input: 32x32x32 | Output: 32x32x64 | RF: 5x5
+            nn.ReLU(),
+            nn.BatchNorm2d(64),
+            nn.Dropout2d(self.drop_prob)
+        )
+        
+        ## Transition Block1
+        self.trans1 = nn.Sequential(
+            nn.Conv2d(64, 32,1, stride=2), # Input: 32x32x64 | Output: 16x16x32 | RF: 5x5
+            nn.ReLU(),
+        )
+
+        ## Convolution Block2
+        self.conv2 =  nn.Sequential(
+            nn.Conv2d(32, 32, 3,  padding=1, bias = False), # Input: 16x16x32 | Output: 16x16x32 | RF: 9x9
+            nn.ReLU(),
+            nn.BatchNorm2d(32),
+            nn.Dropout2d(self.drop_prob),
+
+            ## Depthwise Seperable Convolution1
+            nn.Conv2d(32,32, 3,  padding=1,groups=32 ,bias = False),  # Input: 16x16x32 | Output: 16x16x32 | RF: 13x13
+            nn.Conv2d(32, 64, 1, padding=1, bias = False),   # Input: 16x16x32 | Output: 18x18x64 | RF: 13x13
+            nn.ReLU(),
+            nn.BatchNorm2d(64),
+            nn.Dropout2d(self.drop_prob),
+        )
+        
+        #Transition Block2
+        self.trans2 = nn.Sequential(
+
+            nn.Conv2d(64, 32, 1, stride=2), # Input: 18x18x32 | Output: 9x9x64 | RF: 13x13
+            nn.ReLU()
+        )
+
+        #Convolution Block3
+        self.conv3 = nn.Sequential(
+            
+            ## Dilation Block
+            nn.Conv2d(32, 64, 3,  padding=1, bias = False,dilation=2), # Input: 9x9x64 | Output: 7x7x64 | RF: 29x29
+            nn.ReLU(),
+            nn.BatchNorm2d(64),
+            nn.Dropout2d(self.drop_prob),
+
+            nn.Conv2d(64, 64, 3,  padding=1, bias = False),  # Input: 7x7x64| Output: 7x7x64 | RF: 45x45
+            nn.ReLU(),
+            nn.BatchNorm2d(64),
+            nn.Dropout2d(self.drop_prob),
+        )
+
+        #Transition Block3
+        self.trans3 = nn.Sequential(
+
+            nn.Conv2d(64, 16, 1, stride=2), # Input: 7x7x64| Output: 4x4x16 | RF: 61x61
+            nn.ReLU()
+        )
+
+        #Convolution Block4        
+        self.conv4 = nn.Sequential(
+            nn.Conv2d(16, 32, 3, padding=1, bias = False), # Input: 4x4x16 | Output: 4x4x32 | RF: 93x93
+            nn.ReLU(),
+            nn.BatchNorm2d(32),
+            nn.Dropout2d(self.drop_prob),
+
+            ## Depthwise seperable Convolution2
+            nn.Conv2d(32,32, 3,  padding=1,groups=32 ,bias = False),# Input: 4x4x16 | Output: 4x4x32 | RF: 125x125
+            nn.Conv2d(32, 10, 1, padding=1, bias = False),          # Input: 4x4x32| Output: 6x6x10 | RF: 125x125
+            # nn.ReLU(),
+            # nn.BatchNorm2d(10),
+        )
+
+        ## Output Block
+        self.gap = nn.Sequential(
+            nn.AdaptiveAvgPool2d(1)
+        ) 
+
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.trans1(x)
+
+        x = self.conv2(x) 
+        x = self.trans2(x) 
+
+        x = self.conv3(x) 
+        x = self.trans3(x)
+
+        x = self.conv4(x)
+        x = self.gap(x)
+
+        x = x.view(-1,10)
+        return F.log_softmax(x,dim=1)
